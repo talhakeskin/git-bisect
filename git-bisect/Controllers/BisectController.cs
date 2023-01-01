aee5f57275a93dc8b4ca8b3aa56b38ac737fe79d
Talha Keskin <talhakskn@gmail.com>
2023-01-01 16:53:20 +0300

    initialization of the project

---
+++ git-bisect/Controllers/BisectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace git_bisect.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BisectController : Controller
+    {
+        [HttpGet(Name = "Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
