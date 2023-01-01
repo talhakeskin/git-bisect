c0588f80c6e70c65bb83761c4b67fc8d268ea598
Talha Keskin <talhakskn@gmail.com>
2023-01-01 19:40:33 +0300

    Addition func, commit 1

--- git-bisect/Controllers/BisectController.cs
+++ git-bisect/Controllers/BisectController.cs
@@ -16,7 +16,14 @@ namespace git_bisect.Controllers
                 Y = 2
             };
 
+            result.Addition = this.Addition(result.X, result.Y);
+
             return View(result);
+        }
+
+        private int Addition (int x, int y)
+        {
+            return x + y; // This will contain typo consciously.
         }
     }
 }
